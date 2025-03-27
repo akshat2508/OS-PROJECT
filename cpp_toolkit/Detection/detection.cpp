466600cce2f861da6481e529312c4956d8cab1ff
mansirathor27 <mansirathor575@gmail.com>
2025-03-27 11:26:01 +0530

    Fetching the data from the input file in deadlock detection

--- cpp_toolkit/Detection/detection.cpp
+++ cpp_toolkit/Detection/detection.cpp
@@ -6,3 +6,22 @@ DeadlockDetection::DeadlockDetection(int p, int r) : processes(p), resources(r) 
     need.resize(p, vector<int>(r));
     available.resize(r);
 }
+
+void DeadlockDetection::inputFromFile(ifstream& inFile) {
+    lock_guard<mutex> lock(mtx);
+    
+    for (int i = 0; i < processes; i++)
+        for (int j = 0; j < resources; j++)
+            inFile >> allocation[i][j];
+
+    for (int i = 0; i < processes; i++)
+        for (int j = 0; j < resources; j++)
+            inFile >> maxClaim[i][j];
+
+    for (int j = 0; j < resources; j++)
+        inFile >> available[j];
+
+    for (int i = 0; i < processes; i++)
+        for (int j = 0; j < resources; j++)
+            need[i][j] = maxClaim[i][j] - allocation[i][j];
+}
